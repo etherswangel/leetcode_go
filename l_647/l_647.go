56c22588ebc1a74eb708591eca985233abf50068
Walker <zero.stevewalker@gmail.com>
2022-05-22 14:19:47 +0800

    May22

---
+++ l_647/l_647.go
@@ -0,0 +1,28 @@
+package leetcode
+
+import "fmt"
+
+func countSubstrings(s string) int {
+	res := 0
+	for i := 0; i < len(s); i++ {
+		j := 0
+		for j < len(s)-i && s[i+j] == s[i] {
+			j++
+		}
+		res += (j * (j + 1)) / 2
+		fmt.Println("a", res)
+
+		k := i + j - 1
+		for l := 1; ; l++ {
+			if i-l >= 0 && k+l < len(s) && s[i-l] == s[k+l] {
+				res++
+				fmt.Println("b", res, i, k, l)
+			} else {
+				break
+			}
+		}
+		i += j - 1
+		fmt.Println(res)
+	}
+	return res
+}
---
+++ l_647/l_647_test.go
@@ -0,0 +1,25 @@
+package leetcode
+
+import "testing"
+
+func Test_countSubstrings(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"2",
+			args{"abbcbb"},
+			25},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countSubstrings(tt.args.s); got != tt.want {
+				t.Errorf("countSubstrings() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
