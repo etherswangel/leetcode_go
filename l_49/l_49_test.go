26e9afaf064b12c2ff021e0f51b0d6395cfa41a4
Walker <zero.stevewalker@gmail.com>
2022-10-28 12:36:00 +0800

    Oct.28 test fix

--- l_49/l_49_test.go
+++ l_49/l_49_test.go
@@ -2,6 +2,7 @@ package leetcode
 
 import (
 	"reflect"
+	"sort"
 	"testing"
 )
 
@@ -18,7 +19,20 @@ func Test_groupAnagrams(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := groupAnagrams(tt.args.strs); !reflect.DeepEqual(got, tt.want) {
+			got := groupAnagrams(tt.args.strs)
+			for _, g := range got {
+				sort.Strings(g)
+			}
+			for _, w := range tt.want {
+				sort.Strings(w)
+			}
+			sort.Slice(got, func(i, j int) bool {
+				return got[i][0] < got[j][0]
+			})
+			sort.Slice(tt.want, func(i, j int) bool {
+				return tt.want[i][0] < tt.want[j][0]
+			})
+			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("groupAnagrams() = %v, want %v", got, tt.want)
 			}
 		})
